5c83cbb430065a76a6047dade127792d3c68de70
Ramesh <ramesh.jamakatel@foenloan.com>
2026-01-13 23:24:41 +0545

    Initial commit:topup-service setup

---
+++ src/main/java/com/vanilla/topup/TopupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vanilla.topup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanilla/topup/TopupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanilla.topup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
